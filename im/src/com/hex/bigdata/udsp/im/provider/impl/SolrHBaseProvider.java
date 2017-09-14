9fef51400c109d260cd993ca0f1c1486999fada5
Junjie.M <junjie.miao@goupwith.com>
2017-09-14 09:39:51 +0800

    update

--- im/src/com/hex/bigdata/udsp/im/provider/impl/SolrHBaseProvider.java
+++ im/src/com/hex/bigdata/udsp/im/provider/impl/SolrHBaseProvider.java
@@ -1,18 +1,12 @@
 package com.hex.bigdata.udsp.im.provider.impl;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.hex.bigdata.udsp.common.constant.DataType;
 import com.hex.bigdata.udsp.common.provider.model.Datasource;
 import com.hex.bigdata.udsp.im.provider.RealtimeTargetProvider;
-import com.hex.bigdata.udsp.im.provider.impl.util.SolrUtil;
-import com.hex.bigdata.udsp.im.provider.impl.wrapper.SolrHBaseWrapper;
 import com.hex.bigdata.udsp.im.provider.impl.model.datasource.SolrHBaseDatasource;
 import com.hex.bigdata.udsp.im.provider.impl.wrapper.SolrHBaseWrapper;
 import com.hex.bigdata.udsp.im.provider.model.Metadata;
 import com.hex.bigdata.udsp.im.provider.model.MetadataCol;
 import com.hex.bigdata.udsp.im.provider.model.Model;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.solr.client.solrj.SolrServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,6 +26,7 @@ public class SolrHBaseProvider extends SolrHBaseWrapper implements RealtimeTarge
     private SolrProvider solrProvider;
     @Autowired
     private HBaseProvider hbaseProvider;
+
     private List<MetadataCol> getColumns(String collectionName, String solrServers) {
         return solrProvider.getColumns(collectionName, solrServers);
     }
@@ -44,7 +39,7 @@ public class SolrHBaseProvider extends SolrHBaseWrapper implements RealtimeTarge
         List<MetadataCol> metadataCols = new ArrayList<>();
         // 获取Solr的字段信息
         SolrServer solrServer = getSolrServer(collectionName, solrHBaseDatasource);
-        metadataCols.addAll(getColumns(collectionName,  datasource.getPropertyMap().get("solr.servers").getValue()));
+        metadataCols.addAll(getColumns(collectionName, datasource.getPropertyMap().get("solr.servers").getValue()));
 
 //        HBase无法获取字段信息
 //        metadataCols.addAll(null);
@@ -72,7 +67,7 @@ public class SolrHBaseProvider extends SolrHBaseWrapper implements RealtimeTarge
     }
 
     @Override
-    public void inputData() {
+    public void inputData(Model model) {
 
     }
 }
