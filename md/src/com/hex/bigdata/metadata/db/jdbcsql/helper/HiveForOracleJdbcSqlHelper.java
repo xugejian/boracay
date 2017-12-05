2ce94d7c748cb2c54951ed8b2ea6e0981e1ebd6c
Junjie.M <junjie.miao@goupwith.com>
2017-12-05 16:08:03 +0800

    更新metadata获取impala\hive的元信息

--- md/src/com/hex/bigdata/metadata/db/jdbcsql/helper/HiveForOracleJdbcSqlHelper.java
+++ md/src/com/hex/bigdata/metadata/db/jdbcsql/helper/HiveForOracleJdbcSqlHelper.java
@@ -1,6 +1,7 @@
 package com.hex.bigdata.metadata.db.jdbcsql.helper;
 
 import com.hex.bigdata.metadata.db.jdbcsql.BaseJdbcSqlHelper;
+import com.hex.bigdata.metadata.db.util.Constant;
 import com.hex.bigdata.metadata.db.util.DBType;
 
 import java.sql.Connection;
@@ -25,17 +26,48 @@ public class HiveForOracleJdbcSqlHelper extends BaseJdbcSqlHelper {
 
     @Override
     public String getDatabasesSql() {
-        return null;
+        return "SELECT"
+                + " NAME " + Constant.DB_NAME
+                + " FROM DBS"
+                + " ORDER BY NAME";
     }
 
     @Override
     public String getTablesSql(String dbName) {
-        return null;
+        return "SELECT"
+                + " TAB.TBL_NAME " + Constant.TB_NAME
+                + " FROM TBLS TAB, DBS DB"
+                + " WHERE TAB.DB_ID = DB.DB_ID"
+                + "  AND DB.NAME = '" + dbName.toLowerCase() + "'"
+                + " ORDER BY TAB.TBL_NAME";
     }
 
     @Override
     public String getColumnsSql(String dbName, String tbName) {
-        return null;
+        return "SELECT"
+                + "  COL.\"COMMENT\" " + Constant.COL_COMMENT
+                + ", COL.TYPE_NAME " + Constant.COL_DATA_TYPE
+                + ", COL.COLUMN_NAME " + Constant.COL_NAME
+                + ", COL.INTEGER_IDX + 1 " + Constant.COL_SEQ
+                + ", 0 " + Constant.COL_PF_SEQ
+                + " FROM COLUMNS_V2 COL, DBS DB, TBLS TAB, SDS SDS"
+                + " WHERE COL.CD_ID = SDS.CD_ID"
+                + "  AND TAB.SD_ID = SDS.SD_ID"
+                + "  AND TAB.DB_ID = DB.DB_ID"
+                + "  AND DB.NAME = '" + dbName.toLowerCase() + "'"
+                + "  AND TAB.TBL_NAME = '" + tbName.toLowerCase() + "'"
+                + " UNION ALL "
+                + " SELECT"
+                + "  PF.PKEY_COMMENT " + Constant.COL_COMMENT
+                + ", PF.PKEY_TYPE " + Constant.COL_DATA_TYPE
+                + ", PF.PKEY_NAME " + Constant.COL_NAME
+                + ", 0 " + Constant.COL_SEQ
+                + ", PF.INTEGER_IDX + 1 " + Constant.COL_PF_SEQ
+                + " FROM DBS DB, TBLS TAB, PARTITION_KEYS PF"
+                + " WHERE TAB.DB_ID = DB.DB_ID"
+                + "  AND PF.TBL_ID = TAB.TBL_ID"
+                + "  AND DB.NAME = '" + dbName.toLowerCase() + "'"
+                + "  AND TAB.TBL_NAME = '" + tbName.toLowerCase() + "'";
     }
 
     @Override
