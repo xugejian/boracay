733fc54bba9b46f620fbd6081a3fb024889c9d2e
Junjie.M <junjie.miao@goupwith.com>
2018-04-27 12:21:07 +0800

    update

--- sql/update/20171122/udsp_change_structure.sql
+++
@@ -1,7 +0,0 @@
-
--- 20171122 添加告警方式
--- Add/modify columns
-alter table RC_USER_SERVICE add ALARM_TYPE VARCHAR2(32);
--- Add comments to the columns
-comment on column RC_USER_SERVICE.ALARM_TYPE
-  is '告警方式（NONE、MAIL、...）';
