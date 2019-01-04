d6028d1019e40ffe0968b1c5ae1628170695317b
Junjie.M <junjie.miao@goupwith.com>
2019-01-04 15:47:13 +0800

    添加了2.4版本的SDK使用手册

--- sdk/src/com/hex/bigdata/udsp/jdbc/UdspStatement.java
+++ sdk/src/com/hex/bigdata/udsp/jdbc/UdspStatement.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.hex.bigdata.udsp.common.constant.ConsumerEntity;
 import com.hex.bigdata.udsp.common.constant.ConsumerType;
+import com.hex.bigdata.udsp.common.constant.DataType;
 import com.hex.bigdata.udsp.common.constant.StatusCode;
 import com.hex.bigdata.udsp.jdbc.netty.Client;
 import com.hex.bigdata.udsp.jdbc.netty.ClientFactory;
@@ -21,7 +22,6 @@ import java.util.Map;
  * UDSP JDBC Statement
  */
 public class UdspStatement implements Statement {
-
 
     private final UdspConnection connection;
     private final ClientFactory factory;
@@ -42,51 +42,51 @@ public class UdspStatement implements Statement {
 
     @Override
     public ResultSet executeQuery(String sql) throws SQLException {
-        if (!execute(sql)) {
-            throw new SQLException("The query did not generate a result set!");
+        if (!execute (sql)) {
+            throw new SQLException ("The query did not generate a result set!");
         }
         return resultSet;
     }
 
     @Override
     public int executeUpdate(String sql) throws SQLException {
-        execute(sql);
+        execute (sql);
         return 0;
     }
 
     @Override
     public void close() throws SQLException {
-        if (!isClosed()) {
+        if (!isClosed ()) {
             try {
-                factory.remove(url);
+                factory.remove (url);
             } catch (Exception e) {
-                client.close();
-                e.printStackTrace();
+                client.close ();
+                e.printStackTrace ();
             }
         }
     }
 
     @Override
     public int getMaxFieldSize() throws SQLException {
-        throw new SQLException("Method not supported");
+        throw new SQLException ("Method not supported");
     }
 
     @Override
     public void setMaxFieldSize(int max) throws SQLException {
-        throw new SQLException("Method not supported");
+        throw new SQLException ("Method not supported");
     }
 
     @Override
     public int getMaxRows() throws SQLException {
-        checkConnection("getMaxRows");
+        checkConnection ("getMaxRows");
         return maxRows;
     }
 
     @Override
     public void setMaxRows(int max) throws SQLException {
-        checkConnection("setMaxRows");
+        checkConnection ("setMaxRows");
         if (max < 0) {
-            throw new SQLException("max must be >= 0");
+            throw new SQLException ("max must be >= 0");
         }
         maxRows = max;
     }
@@ -94,29 +94,29 @@ public class UdspStatement implements Statement {
     @Override
     public void setEscapeProcessing(boolean enable) throws SQLException {
         if (enable) {
-            throw new SQLException("Method not supported");
+            throw new SQLException ("Method not supported");
         }
     }
 
     @Override
     public int getQueryTimeout() throws SQLException {
-        checkConnection("getQueryTimeout");
+        checkConnection ("getQueryTimeout");
         return 0;
     }
 
     @Override
     public void setQueryTimeout(int seconds) throws SQLException {
-        throw new SQLException("Method not supported");
+        throw new SQLException ("Method not supported");
     }
 
     @Override
     public void cancel() throws SQLException {
-        throw new SQLException("Method not supported");
+        throw new SQLException ("Method not supported");
     }
 
     @Override
     public SQLWarning getWarnings() throws SQLException {
-        checkConnection("getWarnings");
+        checkConnection ("getWarnings");
         return warningChain;
     }
 
@@ -127,52 +127,59 @@ public class UdspStatement implements Statement {
 
     @Override
     public void setCursorName(String name) throws SQLException {
-        throw new SQLFeatureNotSupportedException("Method not supported");
+        throw new SQLFeatureNotSupportedException ("Method not supported");
     }
 
     @Override
     public boolean execute(String sql) throws SQLException {
-        checkConnection("execute");
+        checkConnection ("execute");
 
-        SqlRequest request = new SqlRequest();
-        request.setServiceName(url.getServiceName());
-        request.setEntity(ConsumerEntity.START.getValue());
-        request.setType(ConsumerType.SYNC.getValue());
-        request.setUdspUser(url.getUsername());
-        request.setToken(url.getPassword());
-        request.setSql(sql);
-        JSONObject jsonObject = (JSONObject) JSON.toJSON(request);
-        String reqJson = jsonObject.toJSONString();
+        SqlRequest request = new SqlRequest ();
+        request.setServiceName (url.getServiceName ());
+        request.setEntity (ConsumerEntity.START.getValue ());
+        request.setType (ConsumerType.SYNC.getValue ());
+        request.setUdspUser (url.getUsername ());
+        request.setToken (url.getPassword ());
+        request.setSql (sql);
+        JSONObject jsonObject = (JSONObject) JSON.toJSON (request);
+        String reqJson = jsonObject.toJSONString ();
         try {
-            String rspJson = client.send(reqJson);
-            SyncResponse response = JSON.parseObject(rspJson, SyncResponse.class);
+            String rspJson = client.send (reqJson);
+            SyncResponse response = JSON.parseObject (rspJson, SyncResponse.class);
             if (response == null) {
-                throw new SQLException("Response result is empty");
+                throw new SQLException ("Response result is empty");
             }
-            if (!StatusCode.SUCCESS.getValue().equals(response.getStatusCode())) {
-                throw new SQLException(response.getMessage());
+            if (!StatusCode.SUCCESS.getValue ().equals (response.getStatusCode ())) {
+                throw new SQLException (response.getMessage ());
             }
-            List<Map<String, String>> records = response.getRecords();
-            LinkedHashMap<String, String> columns = response.getReturnColumns();
+            List<Map<String, String>> records = response.getRecords ();
+            LinkedHashMap<String, String> columns = response.getReturnColumns ();
             List<String> columnNames = null;
             List<String> columnTypes = null;
-            if (columns != null && columns.size() != 0) {
-                columnNames = new ArrayList<>();
-                columnTypes = new ArrayList<>();
-                for (Map.Entry<String, String> entry : columns.entrySet()) {
-                    columnNames.add(entry.getKey());
-                    columnTypes.add(entry.getValue());
+            if (columns != null && columns.size () != 0) { // 有返回字段信息
+                columnNames = new ArrayList<> ();
+                columnTypes = new ArrayList<> ();
+                for (Map.Entry<String, String> entry : columns.entrySet ()) {
+                    columnNames.add (entry.getKey ());
+                    columnTypes.add (entry.getValue ());
+                }
+            } else if (records != null && records.size () != 0) { // 无返回字段信息
+                columnNames = new ArrayList<> ();
+                columnTypes = new ArrayList<> ();
+                for (Map.Entry<String, String> entry : records.get (0).entrySet ()) {
+                    columnNames.add (entry.getKey ());
+                    columnTypes.add (DataType.STRING.getValue ());
                 }
             }
             resultSet = new UdspQueryResultSet
-                    .Builder(this) //
-                    .setClient(client) //
-                    .setColumnNames(columnNames) //
-                    .setColumnTypes(columnTypes) //
-                    .setRecords(records) //
-                    .build();
+                    .Builder (this) //
+                    .setClient (client) //
+                    .setColumnNames (columnNames) //
+                    .setColumnTypes (columnTypes) //
+                    .setRecords (records) //
+                    .build ();
         } catch (Exception e) {
-            throw new SQLException(e);
+            throw new SQLException (e);
         }
 
         return true;
@@ -180,13 +187,13 @@ public class UdspStatement implements Statement {
 
     @Override
     public ResultSet getResultSet() throws SQLException {
-        checkConnection("getResultSet");
+        checkConnection ("getResultSet");
         return resultSet;
     }
 
     @Override
     public int getUpdateCount() throws SQLException {
-        checkConnection("getUpdateCount");
+        checkConnection ("getUpdateCount");
         return -1;
     }
 
@@ -197,121 +204,121 @@ public class UdspStatement implements Statement {
 
     @Override
     public void setFetchDirection(int direction) throws SQLException {
-        checkConnection("setFetchDirection");
+        checkConnection ("setFetchDirection");
         if (direction != ResultSet.FETCH_FORWARD) {
-            throw new SQLException("Not supported direction " + direction);
+            throw new SQLException ("Not supported direction " + direction);
         }
     }
 
     @Override
     public int getFetchDirection() throws SQLException {
-        checkConnection("getFetchDirection");
+        checkConnection ("getFetchDirection");
         return ResultSet.FETCH_FORWARD;
     }
 
     @Override
     public void setFetchSize(int rows) throws SQLException {
-        checkConnection("setFetchSize");
+        checkConnection ("setFetchSize");
         fetchSize = rows;
     }
 
     @Override
     public int getFetchSize() throws SQLException {
-        checkConnection("getFetchSize");
+        checkConnection ("getFetchSize");
         return fetchSize;
     }
 
     @Override
     public int getResultSetConcurrency() throws SQLException {
-        throw new SQLException("Method not supported");
+        throw new SQLException ("Method not supported");
     }
 
     @Override
     public int getResultSetType() throws SQLException {
-        checkConnection("getResultSetType");
+        checkConnection ("getResultSetType");
         return ResultSet.TYPE_FORWARD_ONLY;
     }
 
     @Override
     public void addBatch(String sql) throws SQLException {
-        throw new SQLException("Method not supported");
+        throw new SQLException ("Method not supported");
     }
 
     @Override
     public void clearBatch() throws SQLException {
-        throw new SQLException("Method not supported");
+        throw new SQLException ("Method not supported");
     }
 
     @Override
     public int[] executeBatch() throws SQLException {
-        throw new SQLException("Method not supported");
+        throw new SQLException ("Method not supported");
     }
 
     private void checkConnection(String action) throws SQLException {
-        if (isClosed()) {
-            throw new SQLException("Can't " + action + " after statement has been closed");
+        if (isClosed ()) {
+            throw new SQLException ("Can't " + action + " after statement has been closed");
         }
     }
 
     @Override
     public Connection getConnection() throws SQLException {
-        checkConnection("getConnection");
+        checkConnection ("getConnection");
         return this.connection;
     }
 
     @Override
     public boolean getMoreResults(int current) throws SQLException {
-        throw new SQLFeatureNotSupportedException("Method not supported");
+        throw new SQLFeatureNotSupportedException ("Method not supported");
     }
 
     @Override
     public ResultSet getGeneratedKeys() throws SQLException {
-        throw new SQLFeatureNotSupportedException("Method not supported");
+        throw new SQLFeatureNotSupportedException ("Method not supported");
     }
 
     @Override
     public int executeUpdate(String sql, int autoGeneratedKeys) throws SQLException {
-        throw new SQLException("Method not supported");
+        throw new SQLException ("Method not supported");
     }
 
     @Override
     public int executeUpdate(String sql, int[] columnIndexes) throws SQLException {
-        throw new SQLException("Method not supported");
+        throw new SQLException ("Method not supported");
     }
 
     @Override
     public int executeUpdate(String sql, String[] columnNames) throws SQLException {
-        throw new SQLException("Method not supported");
+        throw new SQLException ("Method not supported");
     }
 
     @Override
     public boolean execute(String sql, int autoGeneratedKeys) throws SQLException {
-        throw new SQLException("Method not supported");
+        throw new SQLException ("Method not supported");
     }
 
     @Override
     public boolean execute(String sql, int[] columnIndexes) throws SQLException {
-        throw new SQLException("Method not supported");
+        throw new SQLException ("Method not supported");
     }
 
     @Override
     public boolean execute(String sql, String[] columnNames) throws SQLException {
-        throw new SQLException("Method not supported");
+        throw new SQLException ("Method not supported");
     }
 
     @Override
     public int getResultSetHoldability() throws SQLException {
-        throw new SQLException("Method not supported");
+        throw new SQLException ("Method not supported");
     }
 
     @Override
     public boolean isClosed() throws SQLException {
-        return client == null || !client.isConnected();
+        return client == null || !client.isConnected ();
     }
 
     @Override
     public void setPoolable(boolean poolable) throws SQLException {
-        throw new SQLException("Method not supported");
+        throw new SQLException ("Method not supported");
     }
 
     @Override
@@ -331,7 +338,7 @@ public class UdspStatement implements Statement {
 
     @Override
     public <T> T unwrap(Class<T> iface) throws SQLException {
-        throw new SQLException("Cannot unwrap to " + iface);
+        throw new SQLException ("Cannot unwrap to " + iface);
     }
 
     @Override
