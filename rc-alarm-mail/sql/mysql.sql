4608be8be174c3cfb4b48a70baeafe2ec3bd7c92
Junjie.M <junjie.miao@goupwith.com>
2019-11-06 14:57:39 +0800

    RC模块拆分和添加结果数据落地控制和添加日期时间窗口控制

---
+++ rc-alarm-mail/sql/mysql.sql
@@ -0,0 +1,27 @@
+-- 添加电子邮件类型
+insert into T_GF_DICT (dict_type_id, dict_id, dict_name, status, sort_no, parent_id, seqno, appid, filter)
+values ('RC_ALARM_TYPE', 'MAIL', '发送邮件', null, 2, null, null, 'default', null);
+
+-- 添加电子邮件的类型配置
+insert into T_GF_DICT_TYPE (dict_type_id, dict_type_name, appid)
+values ('RC_ALARM_PROPS_MAIL', '注册中心-警报参数-MAIL（电子邮件）', 'default');
+insert into T_GF_DICT (dict_type_id, dict_id, dict_name, status, sort_no, parent_id, seqno, appid, filter)
+values ('RC_ALARM_PROPS_MAIL', 'mail.smtp.host', 'SMTP服务器地址（必填）', null, 1, null, null, 'default', null);
+insert into T_GF_DICT (dict_type_id, dict_id, dict_name, status, sort_no, parent_id, seqno, appid, filter)
+values ('RC_ALARM_PROPS_MAIL', 'mail.smtp.port', 'SMTP服务器端口（必填）', null, 2, 'RC_ALARM_PROPS_MAIL', null, 'default', null);
+insert into T_GF_DICT (dict_type_id, dict_id, dict_name, status, sort_no, parent_id, seqno, appid, filter)
+values ('RC_ALARM_PROPS_MAIL', 'mail.smtp.from', '发件人（必填）', null, 3, null, null, 'default', null);
+insert into T_GF_DICT (dict_type_id, dict_id, dict_name, status, sort_no, parent_id, seqno, appid, filter)
+values ('RC_ALARM_PROPS_MAIL', 'mail.smtp.ssl.check.server.identity', '是否检查服务的身份', null, 4, null, null, 'default', 'true');
+insert into T_GF_DICT (dict_type_id, dict_id, dict_name, status, sort_no, parent_id, seqno, appid, filter)
+values ('RC_ALARM_PROPS_MAIL', 'mail.smtp.username', '用户名', null, 5, null, null, 'default', null);
+insert into T_GF_DICT (dict_type_id, dict_id, dict_name, status, sort_no, parent_id, seqno, appid, filter)
+values ('RC_ALARM_PROPS_MAIL', 'mail.smtp.password', '密码', null, 6, null, null, 'default', null);
+insert into T_GF_DICT (dict_type_id, dict_id, dict_name, status, sort_no, parent_id, seqno, appid, filter)
+values ('RC_ALARM_PROPS_MAIL', 'mail.smtp.to', '收件人（必填）', null, 7, null, null, 'default', null);
+insert into T_GF_DICT (dict_type_id, dict_id, dict_name, status, sort_no, parent_id, seqno, appid, filter)
+values ('RC_ALARM_PROPS_MAIL', 'mail.smtp.cc', '抄送', null, 8, null, null, 'default', null);
+insert into T_GF_DICT (dict_type_id, dict_id, dict_name, status, sort_no, parent_id, seqno, appid, filter)
+values ('RC_ALARM_PROPS_MAIL', 'mail.smtp.bcc', '密送', null, 9, null, null, 'default', null);
+insert into T_GF_DICT (dict_type_id, dict_id, dict_name, status, sort_no, parent_id, seqno, appid, filter)
+values ('RC_ALARM_PROPS_MAIL', 'mail.smtp.subject', '主题', null, 10, null, null, 'default', null);
