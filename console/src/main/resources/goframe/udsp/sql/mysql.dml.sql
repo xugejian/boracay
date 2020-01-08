4cad8fbb74d9c6c450c3f056fbb60d003ad9c4d8
Junjie.M <junjie.miao@goupwith.com>
2020-01-08 20:18:52 +0800

    update

---
+++ console/src/main/resources/goframe/udsp/sql/mysql.dml.sql
@@ -0,0 +1,4 @@
+insert into T_GF_MENU (menuid, menuname, menulabel, menucode, isleaf, parameter, displayorder, app_id, menu_action, parentmenuid, menu_icon)
+values ('118011', '操作导航', null, 'navigator', '1', null, 1, 'default', 'udsp.navigator', '118001', 'fa fa-arrow-right');
+insert into T_GF_MENU (menuid, menuname, menulabel, menucode, isleaf, parameter, displayorder, app_id, menu_action, parentmenuid, menu_icon)
+values ('118001', '操作导航', null, 'navigator.core', '2', null, 0, 'default', null, 'root', null);
